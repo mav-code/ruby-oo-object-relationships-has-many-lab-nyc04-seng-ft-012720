aa262c5ff214bdcd4112b87e3aba19ef1d9fb36e
mav-code <mavisansmembres@gmail.com>
2020-02-03 14:07:26 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,19 @@
+class Author
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+    def posts
+        Post.all.select{|post| post.author == self}
+    end
+    def add_post(post)
+        post.author = self
+    end
+    def add_post_by_title(newtitle)
+        newpost = Post.new(newtitle)
+        newpost.author = self
+    end
+    def self.post_count
+        Post.all.length
+    end
+end
